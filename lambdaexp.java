44a0096a8694045b02e1ccf3c25fca7e2e3d49a5
Abhishek <everycodeab@gmail.com>
2026-04-30 14:45:41 +0530

    new changes committed

---
+++ lambdaexp.java
@@ -0,0 +1,19 @@
+
+interface A {
+
+    void display();
+}
+
+// class B extends A {
+//     @Override
+//     public void display() {
+//         System.out.println("This is a lambda expression example using inheritance");
+//     }
+// }
+public class lambdaexp {
+
+    public static void main(String[] args) {
+        A a = () -> System.out.println("This is a lambda expression example using functional interface"); // Lambda expression implementing the display method of interface A
+        a.display();
+    }
+}
