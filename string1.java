ae755138181d7b70b8b7bc7627db672869fada4c
Abhishek <everycodeab@gmail.com>
2026-05-11 19:40:03 +0530

    new changes committed

---
+++ string1.java
@@ -0,0 +1,28 @@
+
+public class string1 {
+
+    public static void main(String[] args) {
+        // String str1 = "Abhi";
+        // String str2 = "Abhi";
+        // System.out.println(str1 == str2); // true, because string literals are interned
+
+        // String str1 = new String("Abhi");
+        // String str2 = new String("Abhi");
+        // System.out.println(str1 == str2); // false, because str1 and str2 are different objects
+        // System.out.println(str1.equals(str2)); // true, because equals() compares content
+        // String str1 = "Abhi";
+        // String str2 = new String("Abhi");
+        // System.out.println(str1 == str2); // false, because str1 is a string literal and str2 is a new String object
+        // System.out.println(str1.equals(str2)); // true, because equals() compares content
+        // String str1 = "Abhi";
+        // String str2 = "ABHI";
+        // System.out.println(str1.equals(str2)); // false, because equals() is case-sensitive
+        // System.out.println(str1.equalsIgnoreCase(str2)); // true, because equalsIgnoreCase() ignores case
+        // String str1 = "Abhi";
+        // String str2 = "Abhi";
+        // System.out.println(str1.compareTo(str2));// 0, because str1 and str2 are equal
+        // String str1 = "Abhi";
+        // String str2 = "ABHi";// System.out.println(str1.compareTo(str2)); // positive value, because 'b' is greater than 'B'
+        // System.out.println(str1.compareTo(str2)); //
+    }
+}
